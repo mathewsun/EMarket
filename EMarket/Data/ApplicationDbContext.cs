59dbe85e0b056448e982979ffce64fce7f84bb0b
Matvey Dubrovin <mathewsun@rambler.ru>
2025-04-22 21:01:11 +0300

    add migration category goods sales

--- EMarket/Data/ApplicationDbContext.cs
+++ EMarket/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EMarket.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EMarket.Data
@@ -8,6 +9,31 @@ namespace EMarket.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Goods> Goods => Set<Goods>();
+        public DbSet<Sales> Sales => Set<Sales>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+               .Entity<Category>()
+               .Property(e => e.Created)
+               .HasDefaultValueSql("now()");
+
+            modelBuilder
+               .Entity<Goods>()
+               .Property(e => e.Created)
+               .HasDefaultValueSql("now()");
+
+            modelBuilder
+               .Entity<Sales>()
+               .Property(e => e.Created)
+               .HasDefaultValueSql("now()");
         }
     }
 }
--- EMarket/Migrations/20250422175255_AddCategoryGoodsSales.Designer.cs
+++ EMarket/Migrations/20250422175255_AddCategoryGoodsSales.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EMarket.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EMarket.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250422175255_AddCategoryGoodsSales")]
+    partial class AddCategoryGoodsSales
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
