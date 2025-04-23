57cfb3958445bdee3efc72e5614db9818341882e
qLepeshka <qlepeshka@gmail.com>
2025-04-23 23:14:48 +0300

    Add GetAllCategories method

---
+++ EMarket/Controllers/GoodsController.cs
@@ -0,0 +1,22 @@
+﻿using EMarket.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMarket.Controllers
+{
+    public class GoodsController : Controller
+    {
+        private GoodsRepository _goodsRepository;
+
+        public GoodsController(GoodsRepository goodsRepository) 
+        {
+            _goodsRepository = goodsRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _goodsRepository.GetAllCategories();
+
+            return View(categories);
+        }
+    }
+}
